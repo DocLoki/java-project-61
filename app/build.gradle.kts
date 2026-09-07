32439bf4d56c7e5c6b20becc2189b95952fc72e8
Artem Salavatov <treword92@gmail.com>
2026-09-07 14:00:28 +0300

    Configure Spotless

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("application")
     id("io.github.ben-manes.versions") version "0.61.0"
+    id("com.diffplug.spotless") version "7.2.1"
 }
 
 group = "hexlet.code"
@@ -9,6 +10,17 @@ version = "1.0-SNAPSHOT"
 application {
     mainClass.set("hexlet.code.App")
 }
+
+spotless {
+    java {
+        importOrder()
+        removeUnusedImports()
+        googleJavaFormat().aosp()
+        formatAnnotations()
+        leadingTabsToSpaces(4)
+    }
+}
+
 repositories {
     mavenCentral()
 }
