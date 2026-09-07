e79acc45671217a93d75a538ba95989bf2e4b7ce
Artem Salavatov <treword92@gmail.com>
2026-09-07 12:14:40 +0300

    Initial project setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("application")
+    id("io.github.ben-manes.versions") version "0.61.0"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
